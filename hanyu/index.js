1debec5939481bf3496450fdd3d48724d2cfc321
javi-b <job1001@alu.ubu.es>
2022-03-24 21:29:38 +0100

    汉语 number pinyin is transformed to accent pinyin

--- hanyu/index.js
+++ hanyu/index.js
@@ -27,9 +27,62 @@ function LoadVocabFiles() {
         dataType: "text",       
         success: function(csv) {
             hsk1 = $.csv.toObjects(csv);
+            hsk1.forEach(AddAccentPinyin);
             $("#start").prop("disabled", false);
         }
     });
+}
+
+/**
+ * Adds accent pinyin string to vocab unit.
+ */
+function AddAccentPinyin(vocab_item) {
+
+    let pinyin_words = vocab_item.pinyin.split(" ");
+    vocab_item.accent_pinyin = "";
+
+    for (let i = 0; i < pinyin_words.length; i++) {
+        accent_pinyin = GetAccentPinyin(pinyin_words[i]);
+        if (i > 0)
+            vocab_item.accent_pinyin = vocab_item.accent_pinyin + " ";
+        vocab_item.accent_pinyin = vocab_item.accent_pinyin + accent_pinyin;
+    }
+}
+
+/**
+ * Gets the accent pinyin version of a specific pinyin word.
+ */
+function GetAccentPinyin(pinyin_word) {
+
+    const vowels = ["a", "o", "e", "i", "u", "v"];
+    const accent_vowels = [
+        "ā", "á", "ǎ", "à",
+        "ō", "ó", "ǒ", "ò",
+        "ē", "é", "ě", "è",
+        "ī", "í", "ǐ", "ì",
+        "ū", "ú", "ǔ", "ù",
+        "ū", "ǘ", "ǚ", "ǜ"
+    ];
+
+    let word = pinyin_word.slice(0, -1);
+    let tone_i = pinyin_word.slice(-1) - 1;
+
+    if (tone_i == -1) // if the word has no tone, returns the word clean
+        return word;
+
+    let vowel_i = -1;
+    for (let i = 0; i < vowels.length; i++) {
+        if (word.indexOf(vowels[i]) > -1) {
+            vowel_i = i;
+            break;
+        }
+    }
+
+    if (vowel_i == -1) // if no vowel is found, returns the word clean
+        return word;
+
+    return (word.replace(vowels[vowel_i],
+            accent_vowels[tone_i + vowel_i * 4]));
 }
 
 /**
@@ -66,7 +119,7 @@ function OnSubmitAnswer() {
 
     $("#good-answer").css("display", "block");
     $("#hanyu").text(hsk1[i].hanyu);
-    $("#pinyin").text(hsk1[i].pinyin);
+    $("#pinyin").text(hsk1[i].accent_pinyin);
 
     $("#next").css("display", "initial");
     $("#next").focus();
@@ -108,7 +161,7 @@ function SetVocabGroupProgress(group_name, value) {
 
     // sets vocab group progress bar label
     let prgr_label = $(".prgr-label." + group_name)[0];
-    $(prgr_label).text(pct + "%");
+    $(prgr_label).text(pct.toFixed(0) + "%");
 }
 
 /**
